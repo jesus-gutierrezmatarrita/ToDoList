63bdefad7f54c8626d9e1346b0bedfc9406adf55
Jesús Gutiérrez <jesus.gutierrezmatarrita@outlook.com>
2022-08-18 20:48:06 -0600

    Added CREATE and READ from DB

--- app.js
+++ app.js
@@ -2,65 +2,81 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date");
+const path = require("path");
+const mongoose = require("mongoose");
 
 const port = 3000;
 
 const app = express();
 
-let items = [];
-let workItems = [];
+mongoose.connect("mongodb://localhost:27017/todolistDB");
 
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+const taskSchema = {
+  name: String,
+};
+
+const Task = mongoose.model("Task", taskSchema);
+
+app.set("view engine", "ejs");
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
 app.use(express.static("public"));
 
-//functionality area
+/*****************functionality area********************/
 
 app.get("/", (req, res) => {
-
   let day = new Date();
+  const workItems = [];
 
-  let options = {
+  const options = {
     weekday: "long",
     day: "numeric",
-    month: "long"
+    month: "long",
   };
 
   day = day.toLocaleDateString("es-CR", options);
 
-  res.render("list", {
-    listTitle: day,
-    newListItems: items
+  Task.find({}, (err, foundTasks) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("list", {
+        listTitle: day,
+        newListItems: foundTasks,
+      });
+    }
   });
 });
 
 app.get("/work", (req, res) => {
   res.render("list", {
     listTitle: "Work List",
-    newListItems: workItems
+    //newListItems: workItems,
   });
 });
 
 app.post("/", (req, res) => {
-  let item = req.body.newTask;
+  const item = new Task({ name: req.body.newTask });
   if (item !== "") {
-    items.push(item);
+    item.save();
   }
 
   res.redirect("/");
 });
 
-app.post("/work", (req,res) => {
-  let item = req.body.newTask;
-  workItems.push(item);
+app.post("/work", (req, res) => {
+  const item = req.body.newTask;
+  //workItems.push(item);
   res.redirect("/work");
 });
 
 app.get("/about", (req, res) => {
   res.render("about");
-})
+});
 
 app.listen(process.env.PORT || port, () => {
   console.log("The server is running");
